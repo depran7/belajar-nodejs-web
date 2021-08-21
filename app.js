63fa00934c4452600386011d47f6aaea4409820c
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2021-08-21 14:08:46 +0700

    simple routing ,request params, request query

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
-const fs = require("fs");
-const http = require("http");
+// const fs = require("fs");
+// const http = require("http");
+// const port = 3000;
+
+// const renderHTML = (path, res) => {
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       res.writeHead(404);
+//       res.write("File Not Found");
+//     } else {
+//       res.write(data);
+//     }
+//     res.end();
+//   });
+// };
+
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, {
+//       "Content-Type": "text/html"
+//     });
+
+//     const url = req.url;
+
+//     switch (url) {
+//       case "/about":
+//         renderHTML("./about.html", res);
+//         break;
+//       case "/contact":
+//         renderHTML("./contact.html", res);
+//         break;
+//       default:
+//         renderHTML("./index.html", res);
+//         break;
+//     }
+//   })
+//   .listen(port, () => {
+//     console.log(`Server is listening on port ${port}...`);
+//   });
+
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const renderHTML = (path, res) => {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write("File Not Found");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-};
+app.get("/", (req, res) => {
+  // res.send("Hello World!");
+  res.sendFile("./index.html", { root: __dirname });
+});
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, {
-      "Content-Type": "text/html"
-    });
+app.get("/about", (req, res) => {
+  // res.send("Halaman About!");
+  res.sendFile("./about.html", { root: __dirname });
+});
 
-    const url = req.url;
+app.get("/contact", (req, res) => {
+  // res.send("Halaman Contact!");
+  res.sendFile("./contact.html", { root: __dirname });
+});
 
-    switch (url) {
-      case "/about":
-        renderHTML("./about.html", res);
-        break;
-      case "/contact":
-        renderHTML("./contact.html", res);
-        break;
-      default:
-        renderHTML("./index.html", res);
-        break;
-    }
-  })
-  .listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-  });
+app.get("/product/:id", (req, res) => {
+  const product = "Product ID : " + req.params.id;
+  const category = "Category ID : " + req.query.category;
+  res.send(product +"<br>"+ category);
+  // res.json(req.params);
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
